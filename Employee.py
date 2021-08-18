1f9be4f0c4feb29ba9cfe733bbd172e3a259dce9
sandhya74 <sandhiya@5gindia.net>
2021-08-18 09:38:19 +0530

    first commit

---
+++ Employee.py
@@ -0,0 +1,19 @@
+
+class Employee:
+  name = "abi"
+  age = 23
+  Id = 109
+
+p1 = Employee()
+print(p1.name)
+print(p1.age)
+print(p1.Id)
+"""
+class Employee:
+    name = "abi"
+    age = 23
+    def display(self):
+    print(self.name,self.age)
+    emp = Employee()
+    emp.display()
+"""
